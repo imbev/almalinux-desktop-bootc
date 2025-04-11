efef1b683b096045700bda249df93af26feda486
Isaac Beverly <imbev@protonmail.com>
2025-04-10 23:12:06 -0500

    Add missing AlmaLinux branding to plasma

--- scripts/plasma-desktop.sh
+++ scripts/plasma-desktop.sh
@@ -6,4 +6,24 @@ dnf install -y \
     @"KDE Plasma Workspaces" \
     falkon
 
+sed -i \
+    's,Image=Next,Image=Alma-default,g' \
+    /usr/share/plasma/look-and-feel/org.kde.breeze.desktop/contents/defaults
+
+sed -i \
+    's,Image=Next,Image=Alma-default,g' \
+    /usr/share/plasma/look-and-feel/org.kde.breezedark.desktop/contents/defaults
+
+sed -i \
+    's,Image=Next,Image=Alma-default,g' \
+    /usr/share/plasma/look-and-feel/org.kde.breezetwilight.desktop/contents/defaults
+
+sed -i \
+    's,start-here-kde-symbolic,fedora-logo-icon,g' \
+    /usr/share/plasma/plasmoids/org.kde.plasma.kickoff/contents/config/main.xml
+
+sed -i \
+    's,background=/usr/share/wallpapers/Next/contents/images/5120x2880.png,background=/usr/share/wallpapers/Alma-default/contents/images/3840x2160.png,g' \
+    /usr/share/sddm/themes/breeze/theme.conf
+
 systemctl enable sddm
