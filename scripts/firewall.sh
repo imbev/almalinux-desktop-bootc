03ecb3dfbe2389ddec7f42dabfaa922b123f22b5
Isaac Beverly <imbev@protonmail.com>
2025-04-19 17:58:53 -0500

    Add Workstation firewalld config

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -5,3 +5,16 @@ set -euo pipefail
 dnf install -y \
 	firewall-config
 
+cat << EOF > /usr/lib/firewalld/zones/Workstation.xml
+<?xml version="1.0" encoding="utf-8"?>
+<zone>
+  <short>Workstation</short>
+  <description>Unsolicited incoming network packets are rejected from port 1 to 1024, except for select network services. Incoming packets that are related to outgoing network connections are accepted. Outgoing network connections are allowed.</description>
+  <service name="dhcpv6-client"/>
+  <service name="ssh"/>
+  <service name="samba-client"/>
+  <port protocol="udp" port="1025-65535"/>
+  <port protocol="tcp" port="1025-65535"/>
+  <forward/>
+</zone>
+EOF
